a03c8842b4e475e4dfd9c8172158d42de3c496c3
milkyicedtea <81954613+milkyicedtea@users.noreply.github.com>
2025-02-10 20:38:58 +0000

    Updated Fastapi configurations

--- Python/FastAPI/main.py
+++ Python/FastAPI/main.py
@@ -1,21 +1,28 @@
+import asyncio
 import time
 from fastapi import FastAPI
+from hypercorn.asyncio import serve
+from hypercorn.config import Config
 
 app = FastAPI()
 
-
-@app.get("/users")
-async def root():
-    start_time = time.time()
-    users = [
-        {
+@app.get("/test/get-users")
+async def get_users()-> list[dict[str, str | int]]:
+    start = time.perf_counter()
+    users = [{
             "id": user,
             "username": f"user{user}",
             "email": f"user{user}@gmail.com",
             "password": f"password{user}",
-        }
-    for user in range(10000)]
-    execution_time = time.time() - start_time  # End timer
-    print(f"Endpoint executed in {execution_time:.4f} seconds.")
+    } for user in range(10000)]
+    print(f"Execution time: {(time.perf_counter() - start) *1000:3f}ms")
+    return users
+    
+async def run():
+    config = Config()
+    config.bind = ["0.0.0.0:8100"]
+    await serve(app, config=config)
 
-    return {"users": users}
+if __name__ == "__main__":
+    asyncio.run(run())
+    
