53b7887fec19256467af03317fdcedc6d2611623
James Daniel <114611694+jamesdaniel3@users.noreply.github.com>
2025-06-27 15:22:14 -0400

    updated the location of the image files and removed the autogenerated react svg

--- result-display/src/constants/LanguageDataFiles.ts
+++ result-display/src/constants/LanguageDataFiles.ts
@@ -3,10 +3,10 @@ import goData from "../../../wrk-results/gochi-app_benchmark_user-db-read.json";
 import rustdata from "../../../wrk-results/rustaxum-app_benchmark_user-json.json";
 import zigData from "../../../wrk-results/zigzap-app_benchmark_user-json.json";
 
-import cppImage from "../../public/c++.png";
-import goImage from "../../public/go.svg";
-import rustImage from "../../public/rust.png";
-import zigImage from "../../public/zig.svg";
+import cppImage from "../assets/c++.png";
+import goImage from "../assets/go.svg";
+import rustImage from "../assets/rust.png";
+import zigImage from "../assets/zig.svg";
 
 // I haven't figured out why there are multiple Go files yet so I am leaving them out for now
 export const LanguageDataFiles = [
